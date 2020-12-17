78217c1c84805d279174a9143c58112e7d48e6bd
Cybernite21 <Cybernite21@users.noreply.github.com>
2020-12-17 16:45:15 -0600

    Added VFX Canvas and red rim when health is low
    Added VFX Canvas and red rim when health is low

---
+++ Assets/Scripts/DamageVFX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageVFX : MonoBehaviour
+{
+    PlayerControler plr;
+    Color clr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        plr = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControler>();
+        clr = gameObject.GetComponent<RawImage>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        clr = new Color(clr.r, clr.g, clr.b, Mathf.Lerp(1, 0, (float)plr.health / 100f));
+        gameObject.GetComponent<RawImage>().color = clr;
+    }
+}
